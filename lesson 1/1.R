4012dfaaa3f773d35ad6c4318b4069fe6e90955b
sp8sp8 <150517360+sp8sp8@users.noreply.github.com>
2023-11-18 10:47:48 +0300

    Update 1.R

--- lesson 1/1.R
+++ lesson 1/1.R
@@ -1,1 +1,13 @@
+x <- 22
+y <- 8
+a <- 'Hello world'
+z <- x/y
+round(z, digits = 2)  
+x > y 
+cat(a, a, a, sep = ', ')
+
+n<- 2020
+while(n <= 2022) {    
+print(cat('"Маркетинговый проект запустился в ',
+(n<-(n+1)),'году" '), sep='\n')}
 
